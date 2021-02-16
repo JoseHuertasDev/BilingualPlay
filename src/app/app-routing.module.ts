a975682c181df200d85a36132bc477ec736120f7
JoseHuertasDev <70613520+JoseHuertasDev@users.noreply.github.com>
2021-02-16 19:36:27 -0300

    Included bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChooseVideoComponent } from './choose-video/choose-video.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/choose-video/choose-video.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-choose-video',
+  templateUrl: './choose-video.component.html',
+  styleUrls: ['./choose-video.component.scss']
+})
+export class ChooseVideoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
