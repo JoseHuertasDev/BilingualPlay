7854982ceb65af629cf9eb278c5096c01c24797f
JoseHuertasDev <70613520+JoseHuertasDev@users.noreply.github.com>
2021-02-18 13:24:15 -0300

    Added: ChooseVideoComponent, subtitlefileinputcomponent and videofileinputcomponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChooseVideoComponent } from './choose-video/choose-video.component';
+import { SubtitleFileInputComponent } from './subtitle-file-input/subtitle-file-input.component';
+import { VideoFileInputComponent } from './video-file-input/video-file-input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChooseVideoComponent
+    ChooseVideoComponent,
+    SubtitleFileInputComponent,
+    VideoFileInputComponent,
   ],
   imports: [
     BrowserModule,
