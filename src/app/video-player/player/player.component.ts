0cf65490d32ded2cc16b02430837eef6b1a9208d
JoseHuertasDev <70613520+JoseHuertasDev@users.noreply.github.com>
2021-02-24 15:27:20 -0300

    Added:  Added WebChimera
    Added webchimerajs and webchimera render to the project and play a video with mock data en player component

--- src/app/video-player/player/player.component.ts
+++ src/app/video-player/player/player.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-player',
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./player.component.scss']
 })
 export class PlayerComponent implements OnInit {
-
-  constructor() { }
+  //(<any>window).require Allow us to get the native node modules
+  webChimera = (<any>window).require("webchimera.js")
+  renderer = (<any>window).require("wcjs-renderer");
+  player: any;
+  @ViewChild("CanvasPlayer") CanvasPlayer!: ElementRef<HTMLCanvasElement>;
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
-
+  ngAfterViewInit(): void{
+    let canvasNative = this.CanvasPlayer.nativeElement;
+    this.player = this.webChimera.createPlayer();
+    let options = { /* Add renderer options here */ }
+    this.renderer.bind(canvasNative, this.player, options);
+    this.player.play("http://archive.org/download/CartoonClassics/Krazy_Kat_-_Keeping_Up_With_Krazy.mp4");
+    this.player.mute = true;
+  }
+  ngOnDestroy(): void{
+    this.player.stop();
+    this.player.close(); // Closes the video when destroy the component
+  }
 }
