f1e145aa8460ea9e24bdc807c123b8fc02e58606
JoseHuertasDev <70613520+JoseHuertasDev@users.noreply.github.com>
2021-03-11 15:20:54 -0300

    Added: Subtitle service

---
+++ src/app/services/subtitle/subtitle.service.ts
@@ -0,0 +1,67 @@
+import { Injectable } from '@angular/core';
+import { GenericResult } from '@HelpersElectron/GenericResult';
+import { FileModel } from '@Models/FileModel';
+import { IpcService } from '../ipc/ipc.service';
+import { Constants } from '@HelpersElectron/Constants';
+import { DataSubtitle } from '@ClassesElectron/DataSubtitle';
+import { PlayerService } from '../player/player.service';
+import { BehaviorSubject, interval, Subscription } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SubtitleService {
+  private subtitleFile: FileModel | undefined;
+  private _playerService: PlayerService;
+  private _ipcService: IpcService;
+  private currentSubtitle: string | undefined = undefined;
+
+  currentSubBehavior = new BehaviorSubject(this.currentSubtitle);
+
+
+  private mainSubs?: Array<DataSubtitle>;
+
+  intervalCurrentVideo: Subscription | undefined;
+
+  constructor(ipcService: IpcService, playerService: PlayerService) {
+    this._ipcService = ipcService;
+    this._playerService = playerService;
+
+  }
+  private timeoutCurrentVideo(): void{ //Every 3s updates the current time
+    this.intervalCurrentVideo = interval(1000).subscribe(() => { //Updates currentTime periodically
+      this.UpdateSubitleFromTime(this._playerService._currentTime);
+    })
+  }
+  private UpdateSubitleFromTime(currentTime: number){
+    this.mainSubs?.filter((data)=>{
+      let startTime = data.start?data.start: 0;
+      let endTime = data.end?data.end: 0;
+      if(currentTime >= startTime && currentTime<endTime){
+        if(this.currentSubtitle != data.text )
+          this.currentSubtitle = data.text;
+      }
+      else
+        this.currentSubtitle = undefined;
+
+      return this.currentSubBehavior.next(this.currentSubtitle);
+    })
+  }
+  playSubs(){
+    this.timeoutCurrentVideo();
+  }
+  async setFile(subtitleFile: FileModel): Promise<void>{
+    this.subtitleFile = subtitleFile;
+
+    if(subtitleFile.route){
+      let response = await this._ipcService.send< GenericResult<Array<DataSubtitle>> >
+      (Constants.SUBTITLE_CHANNEL,
+      {
+        params: [subtitleFile.route]
+      });
+      if(response.errors)
+        this.mainSubs = response.value;
+      console.log(response.value);
+     }
+  }
+}
