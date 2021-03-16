ffab0cf16a646584a84306fe08eb485f68529020
JoseHuertasDev <70613520+JoseHuertasDev@users.noreply.github.com>
2021-03-16 14:16:08 -0300

    Added: Changed the video to have a property of mainsubs and secondary subs

--- src/app/Models/DashboardSettings.ts
+++ src/app/Models/DashboardSettings.ts
@@ -1,11 +1,10 @@
 import { VideoPlayerModule } from "../video-player/video-player.module";
 import { Language } from "./language";
 import { FileModel } from "./FileModel";
+import {  VideoModel } from "./VideoModel";
 
 export class DashboardSettings {
-  video: FileModel = new FileModel();
-  mainSubs: FileModel = new FileModel();
-  scondarySubs: FileModel  = new FileModel();
+  video: VideoModel = new VideoModel();
   mainLanguage: Language = new Language();
   secondaryLanguage: Language = new Language();
 }
